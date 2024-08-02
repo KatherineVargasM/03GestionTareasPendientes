6023a92e0a821b718652e3b9dfd9a100183dde52
D E L L <D E L L@DESKTOP-RMFHFV2>
2024-08-02 00:27:03 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace GestionTareasPendientes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregarTarea = new System.Windows.Forms.Button();
+            this.btnEliminarTarea = new System.Windows.Forms.Button();
+            this.lstTareas = new System.Windows.Forms.ListBox();
+            this.txtTarea = new System.Windows.Forms.TextBox();
+            this.lbl_Titulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAgregarTarea
+            // 
+            this.btnAgregarTarea.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnAgregarTarea.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregarTarea.Location = new System.Drawing.Point(305, 125);
+            this.btnAgregarTarea.Name = "btnAgregarTarea";
+            this.btnAgregarTarea.Size = new System.Drawing.Size(152, 45);
+            this.btnAgregarTarea.TabIndex = 0;
+            this.btnAgregarTarea.Text = "Agregar Tarea";
+            this.btnAgregarTarea.UseVisualStyleBackColor = false;
+            this.btnAgregarTarea.Click += new System.EventHandler(this.btnAgregarTarea_Click);
+            // 
+            // btnEliminarTarea
+            // 
+            this.btnEliminarTarea.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnEliminarTarea.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarTarea.Location = new System.Drawing.Point(305, 378);
+            this.btnEliminarTarea.Name = "btnEliminarTarea";
+            this.btnEliminarTarea.Size = new System.Drawing.Size(152, 45);
+            this.btnEliminarTarea.TabIndex = 1;
+            this.btnEliminarTarea.Text = "Eliminar Tarea";
+            this.btnEliminarTarea.UseVisualStyleBackColor = false;
+            this.btnEliminarTarea.Click += new System.EventHandler(this.btnEliminarTarea_Click);
+            // 
+            // lstTareas
+            // 
+            this.lstTareas.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstTareas.FormattingEnabled = true;
+            this.lstTareas.ItemHeight = 16;
+            this.lstTareas.Location = new System.Drawing.Point(229, 198);
+            this.lstTareas.Name = "lstTareas";
+            this.lstTareas.Size = new System.Drawing.Size(310, 164);
+            this.lstTareas.TabIndex = 3;
+            // 
+            // txtTarea
+            // 
+            this.txtTarea.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTarea.Location = new System.Drawing.Point(229, 84);
+            this.txtTarea.Name = "txtTarea";
+            this.txtTarea.Size = new System.Drawing.Size(310, 23);
+            this.txtTarea.TabIndex = 4;
+            // 
+            // lbl_Titulo
+            // 
+            this.lbl_Titulo.AutoSize = true;
+            this.lbl_Titulo.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lbl_Titulo.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Titulo.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lbl_Titulo.Location = new System.Drawing.Point(272, 28);
+            this.lbl_Titulo.Name = "lbl_Titulo";
+            this.lbl_Titulo.Size = new System.Drawing.Size(237, 22);
+            this.lbl_Titulo.TabIndex = 5;
+            this.lbl_Titulo.Text = "TAREAS PENDIENTES";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_Titulo);
+            this.Controls.Add(this.txtTarea);
+            this.Controls.Add(this.lstTareas);
+            this.Controls.Add(this.btnEliminarTarea);
+            this.Controls.Add(this.btnAgregarTarea);
+            this.Name = "Form1";
+            this.Text = "GestionTareasPendientes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregarTarea;
+        private System.Windows.Forms.Button btnEliminarTarea;
+        private System.Windows.Forms.ListBox lstTareas;
+        private System.Windows.Forms.TextBox txtTarea;
+        private System.Windows.Forms.Label lbl_Titulo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GestionTareasPendientes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregarTarea_Click(object sender, EventArgs e)
+        {
+            if (txtTarea.Text == "")
+            {
+                MessageBox.Show("Error. Ingresa una tarea.");
+            }
+            else
+            {
+                lstTareas.Items.Add(txtTarea.Text);
+                txtTarea.Clear();
+            }
+        }
+
+        private void btnEliminarTarea_Click(object sender, EventArgs e)
+        {
+            if (lstTareas.SelectedIndex != -1)
+            {
+                DialogResult dialogResult =
+                   MessageBox.Show("¿Esta seguro que desea eliminar esta tarea?", "Tarea",
+                   MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    lstTareas.Items.RemoveAt(lstTareas.SelectedIndex);
+                    MessageBox.Show("Se elimino con exito.");
+                }
+                else
+                {
+                    MessageBox.Show("Se cancelo la operacion.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error. Selecciona una tarea para eliminar.");
+            }
+        }
+    }
+}
